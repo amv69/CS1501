280df24998663141af1b53fecbeb91e043d97900
Alex Visbisky <amv69+@pitt.edu>
2019-07-24 14:59:52 -0400

    Create Assignment5.java

---
+++ Assignment 5/Assignment5.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Assignment5{
+	
+	 adjacencyList = new LinkedList[];
+
+	public static void main(String args[]){
+		File file = new File(args[0]);
+		Scanner sc = new Scanner(file);
+		int numVertex = sc.nextLine();
+		int numEdge = sc.nextLine();
+		while( sc.hasNextLine() ){
+
+		}
+	}
+}
