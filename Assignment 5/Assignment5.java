800a478c935b059ea8a6d045a3b3fd5062365051
Alex Visbisky <amv69+@pitt.edu>
2019-07-24 16:29:35 -0400

    Update Assignment5.java

--- Assignment 5/Assignment5.java
+++ Assignment 5/Assignment5.java
@@ -3,15 +3,33 @@ import java.util.*;
 
 public class Assignment5{
 	
-	 adjacencyList = new LinkedList[];
+	 
+	 int v;
+	 int w;
+	 int weight;
+	 Edge E;
+	 LinkedList<Integer> adjacencyList[];
 
 	public static void main(String args[]){
 		File file = new File(args[0]);
 		Scanner sc = new Scanner(file);
 		int numVertex = sc.nextLine();
 		int numEdge = sc.nextLine();
-		while( sc.hasNextLine() ){
-
+		adjacencyList[] = new LinkedList[numVertex];
+		for(int i = 0; i < numVertex; i++){
+			adjacencyList[i] = new LinkedList<>();
 		}
+		while( sc.hasNextLine() ){
+			v = sc.nextInt();
+			w = sc.nextInt();
+			weight = sc.nextInt();
+			addEdge(v, w, weight);
+		}
+	}
+
+	public void addEdge(int v, int w, int weight){
+		Edge e = new Edge(v, w, weight);
+		adjacencyList[v].addFirst(e);
+		adjacencyList[w].addFirst(e); //Since it is undirected
 	}
 }
